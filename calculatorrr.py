dacbbb497c96b20219dc33eb7a144ce90604f411
Caduct <112777467+Caductious@users.noreply.github.com>
2022-11-30 23:51:41 +0300

    Update calculatorrr.py

--- calculatorrr.py
+++ calculatorrr.py
@@ -1,13 +1,11 @@
 import sys
-def summ():
-  x=[int(number) for number in sys.argv[1:]]
-  return(sum(x))
-def both():
-    lst=[int(number) for number in sys.argv[2:]]
-    if (sys.argv[1])=='+':
-        print(sum(lst))
-    elif (sys.argv[1])=='*':
-        result=1
-        for number in lst:
-            result*=number
-        return(result)
+lst=[int(number) for number in sys.argv[2:]]
+print(lst)
+print(sys.argv[1])
+if (sys.argv[1])=='+':
+  print(sum(lst))
+elif (sys.argv[1])=='x':
+  result=1
+  for number in lst:
+    result*=number
+  print(result)
